85726bb98104ccfad0ea3f51b38d506ea20494fc
David <davidspader97@gmail.com>
2022-09-01 14:59:07 -0300

    add apiresource to routes

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('books', [\App\Http\Controllers\API\BooksController::class, 'index']);
-Route::post('books', [\App\Http\Controllers\API\BooksController::class, 'store']);
-Route::get('books/{id}', [\App\Http\Controllers\API\BooksController::class, 'show']);
-Route::match(['put', 'patch'], 'books/{id}', [\App\Http\Controllers\API\BooksController::class, 'update']);
-Route::delete('books/{id}', [\App\Http\Controllers\API\BooksController::class, 'destroy']);
+Route::apiResource('books', \App\Http\Controllers\API\BooksController::class);
